07007de23ceb1a9fe6bf61d163a480a95e93dd7b
Artemis-Arrow <69902518+Artemis-Arrow@users.noreply.github.com>
2022-08-07 23:37:48 +0300

    blah blah blah

--- EncryptionCracker.py
+++ EncryptionCracker.py
@@ -9,13 +9,13 @@ rand = random.randint(1, 100)
 rando = random.randint(1, 1000)
 z = 5
 for i in range(20):
-        sys.stdout.write("\rDecrypting: " + str(z) +"%")
-        z += 5
-        time.sleep(1)
+    sys.stdout.write("\rDecrypting: " + str(z) +"%")
+    z += 5
+    time.sleep(1)
 if rand == 100:
-        sys.stdout.write("Decryption failed, exiting")
-        exit("Error Code: " + str(rando))
+    sys.stdout.write("Decryption failed, exiting")
+    exit("Error Code: " + str(rando))
 else:                                                                      for j in range(10000):                                                     print(str(hex(random.randint(0, 255))), end = '')
-        letters = string.ascii_letters
-        sys.stdout.write("\nDecryption successful, result is:\n")
-        sys.stdout.write(''.join(random.choice(letters) for i in range(25)))
+    letters = string.ascii_letters
+    sys.stdout.write("\nDecryption successful, result is:\n")
+    sys.stdout.write(''.join(random.choice(letters) for i in range(25)))
