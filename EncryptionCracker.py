0a8726b70c8a39810edca4dc9597b4215459dfb9
Artemis-Arrow <69902518+Artemis-Arrow@users.noreply.github.com>
2022-08-08 07:10:18 +0300

    I made the code run with a roll chance to improve decryption

--- EncryptionCracker.py
+++ EncryptionCracker.py
@@ -7,6 +7,7 @@ encryption = input("enter what you want to decrypt:\n")
 
 rand = random.randint(1, 100)
 rando = random.randint(1, 1000)
+roll_chance = random.randint(1, 10000)
 z = 5
 for i in range(20):
     sys.stdout.write("\rDecrypting: " + str(z) +"%")
@@ -15,6 +16,9 @@ for i in range(20):
 if rand == 100:
     sys.stdout.write("Decryption failed, exiting")
     exit("Error Code: " + str(rando))
+elif roll_chance == 9999:
+    import webbrowser
+    webbrowser.open("https://m.youtube.com/watch/dQw4w9WgXcQ", new = 2)
 else:
     for j in range(10000):
         print(str(hex(random.randint(0, 255))), end = '')
